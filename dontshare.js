0d375063eaf07521df48e61e35e3397487c0ae77
root <root@superchad.xyz>
2024-04-05 15:27:27 +0300

    this file is repaired!

---
+++ dontshare.js
@@ -0,0 +1,37 @@
+const API_ID = '' // your telegram API ID
+
+const API_HASH = '' //Telegram: API Hash
+
+const CANNEL_NAME = 'https://t.me/testingbottesting' // Channel name to moniter
+
+const BUY_AMOUNT = 0.001000
+
+const BOT_TOKEN = '' // https://t.me/
+
+const PRIVATE_KEY = '4xuKiQxGLuzXbGDa2zWXu5Ve5QyAD7cyitv3PYu9c..' // your phantom wallet private key
+
+const PUBLIC_KEY = '7ZAR6hsFP44Ey5J....'
+
+const SIGNAL_KEY = 'BUY ALERT'
+
+const TOKEN_KEY = 'Token Contract:'
+
+const SLIPPAGE = 5
+
+const GAIN_PERCENT = 20
+
+const LOSS_PERCENT = 5
+
+const BIRDEYE_KEY = '9ade21aa6dac44dba98a80e1b5a1d9e6' // your birdeye key
+
+const SOLANA_RPC_URL = "https://api.mainnet-beta.solana.com"
+
+const config = {
+    private_key: PRIVATE_KEY,
+    rpc_url: SOLANA_RPC_URL,
+};
+  
+export default config;
+// module.exports = {
+//     config
+// }
